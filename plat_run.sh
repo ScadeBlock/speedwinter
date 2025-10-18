560be6329f1fc9841e664d16802d7f100f0cf6b4
D <89845150+ScadeBlock@users.noreply.github.com>
2025-10-18 19:31:10 +0700

    Create plat_run.sh

---
+++ plat_run.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt install python3-pip
+pip install urllib3 pystyle rich requests pycryptodome latest_user_agents
+curl https://raw.githubusercontent.com/ScadeBlock/winterBot/refs/heads/main/uview.obf.py -o uview.obf.py
+python uview.obf.py
