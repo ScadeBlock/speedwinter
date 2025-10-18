bdd92abe7b2542b5749d8b2f3a2455668d60f0cf
D <89845150+ScadeBlock@users.noreply.github.com>
2025-10-18 19:35:48 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-pip install urllib3 pystyle rich requests pycryptodome latest_user_agents
+pip install urllib3 pystyle rich requests pycryptodome latest_user_agents httpx regex emoji
 python uview.obf.py
