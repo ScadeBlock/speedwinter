d39e3147f0ae46827b8e7229c63a8d969da92684
D <89845150+ScadeBlock@users.noreply.github.com>
2025-12-28 20:01:12 +0700

    Create plat_rundu.sh

---
+++ plat_rundu.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt install python3-pip
+pip install requests colorama
+curl https://raw.githubusercontent.com/ScadeBlock/winterBot/refs/heads/main/zkfr.py -o zkfr.py
+python zkfr.py
