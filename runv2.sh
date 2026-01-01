dbe0173a6438fbfb5592cc8928d4e647e1c96aca
D <89845150+ScadeBlock@users.noreply.github.com>
2026-01-01 08:25:56 +0700

    Update runv2.sh

--- runv2.sh
+++ runv2.sh
@@ -1,4 +1,4 @@
 python3 -m venv myenv
 source myenv/bin/activate
-pip install urllib3 pystyle rich requests pycryptodome latest_user_agents emoji regex
+pip install urllib3 pystyle rich requests pycryptodome latest_user_agents emoji regex colorama
 python uview.obf.py
